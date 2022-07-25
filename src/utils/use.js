ddd9277f7514f7405ee06ceb1cfa8174cc4418de
dengzemiao <dengzemiaovip@163.com>
2022-07-25 18:11:04 +0800

    2022-07-25 18:11:04 提交优化

--- src/utils/use.js
+++ src/utils/use.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 
 // uView
-// import uView from 'uview-ui'
+import uView from 'uview-ui'
 
 // 使用 uView
-// Vue.use(uView)
+Vue.use(uView)
+// Easycom 配置（参考：https://www.uviewui.com/components/npmSetting.html）
+// 配置 uView 尺寸单位（默认：px，参考：https://www.uviewui.com/components/setting.html）
+uni.$u.config.unit = 'rpx'
