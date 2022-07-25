c876b8c596c74b6711d967fff6ee2df7db47da06
dengzemiao <dengzemiaovip@163.com>
2022-07-25 17:20:57 +0800

    初始化uview

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 
-Vue.config.productionTip = false
+// 导入插件配置
+import '@/utils/use.js'
 
+// 阻止 vue 在启动时生成生产提示
+Vue.config.productionTip = false
+// 删除运行 h5 会报错
 App.mpType = 'app'
 
 const app = new Vue({
