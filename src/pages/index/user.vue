1b4f6cf3872ec016c7fb48ce0f70a5bc200da926
dengzemiao <dengzemiaovip@163.com>
2022-07-27 10:22:14 +0800

    2022-07-27 10:22:14 提交优化

---
+++ src/pages/index/user.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="content">
+		<!-- 框架案例 -->
+		<navigator url="/pages/test/index"><button>框架案例</button></navigator>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'Hello World!'
+		}
+	},
+	onLoad() {
+	},
+	methods: {
+	}
+}
+</script>
+
+<style>
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+button {
+	margin-top: 30rpx;
+	width: calc(100vw - 50rpx);
+}
+</style>
