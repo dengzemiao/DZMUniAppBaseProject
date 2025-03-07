8d4ce3ab13847d3c914ffd801705627655c5fafa
dengzemiao <dengzemiaovip@163.com>
2025-03-07 11:46:10 +0800

    2025-03-07 11:46:06 提交优化

--- src/pages/index/case.vue
+++ src/pages/index/case.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="content">
 		<!-- 框架案例 -->
-		<navigator url="/pages/test/index"><button>框架案例</button></navigator>
+		<navigator url="/pages/case/index"><button>框架案例</button></navigator>
 	</view>
 </template>
 
