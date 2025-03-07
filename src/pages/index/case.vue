75fbd7b0b160ff0e7f6ed10f3dff1b850e9a4547
dengzemiao <dengzemiaovip@163.com>
2025-03-07 10:46:48 +0800

    2025-03-07 10:46:47 提交优化

--- src/pages/index/case.vue
+++ src/pages/index/case.vue
@@ -2,11 +2,23 @@
 	<view class="content">
 		<!-- 框架案例 -->
 		<navigator url="/pages/test/index"><button>框架案例</button></navigator>
+		<!-- uni-ui -->
+		<uni-badge text="1"></uni-badge>
+		<uni-badge text="2" type="success" @click="bindClick"></uni-badge>
+		<uni-badge text="3" type="primary" :inverted="true"></uni-badge>
 	</view>
 </template>
 
 <script>
+// 组件演示文档：https://ext.dcloud.net.cn/plugin?id=55 文档里面找一下，在下面点
+// npm 安装文档：https://www.npmjs.com/package/@dcloudio/uni-ui
+// 因为已经在 pages.json 中注册了 easycom 组件，所有不需要单个引入了
+// import {uniBadge} from '@dcloudio/uni-ui'
+
 export default {
+	// components: {
+	// 	uniBadge
+	// },
 	data () {
 		return {
 			title: 'Hello World!'
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  transpileDependencies:['@dcloudio/uni-ui'],
   devServer: {
 		disableHostCheck: true,
     proxy: {
